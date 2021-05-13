de535eca4764497f8885393c8f6131277311e0a1
Roger <tanimowr@mcmaster.ca>
2021-05-13 08:08:20 -0400

    Updated piece definitions, implemented basic pawns.

--- piece.py
+++ piece.py
@@ -11,12 +11,22 @@ class Piece(ABC):
         '''
         self.name = name
         self.colour = name[0]
+        self.moved = False  # Whether or not the piece has been moved yet
+        self.pos = (-1, -1)
 
     def getName(self):
         return self.name
 
     def getColour(self):
         return self.colour
+
+    def move(self):
+        self.moved = True
+        return
+
+    def setPos(self, r, c):
+        self.pos = (r, c)
+        return
 
     @abstractmethod
     def checkValidMoves(self, gs):
@@ -26,30 +36,64 @@ class Piece(ABC):
         pass
 
 class Pawn(Piece):
-    def checkValidMoves(self, gs):
-        pass
+    def checkValidMoves(self, board):
+        validMoves = []
+        # White Pawn
+        if self.name[0] == "w":
+            if board[self.pos[0]-1][self.pos[1]].getName() == "--":
+                validMoves.append((self.pos[0]-1, self.pos[1]))
+            if self.pos[0] > 0:
+                if board[self.pos[0]-1][self.pos[1]-1].getName()[0] == "b":
+                    validMoves.append((self.pos[0]-1, self.pos[1]-1))
+            if self.pos[0] < 7:
+                if board[self.pos[0]-1][self.pos[1]+1].getName()[0] == "b":
+                    validMoves.append((self.pos[0]+1, self.pos[1]-1))
+            if not self.moved:
+                if board[self.pos[0]-2][self.pos[1]].getName() == "--":
+                    validMoves.append((self.pos[0]-2, self.pos[1]))
+        # Black Pawn
+        else:
+            if board[self.pos[0]+1][self.pos[1]].getName() == "--":
+                validMoves.append((self.pos[0]+1, self.pos[1]))
+            if self.pos[0] > 0:
+                if board[self.pos[0]+1][self.pos[1]-1].getName()[0] == "w":
+                    validMoves.append((self.pos[0]+1, self.pos[1]-1))
+            if self.pos[0] < 7:
+                if board[self.pos[0]+1][self.pos[1]+1].getName()[0] == "w":
+                    validMoves.append((self.pos[0]+1, self.pos[1]+1))
+            if not self.moved:
+                if board[self.pos[0]+2][self.pos[1]].getName() == "--":
+                    validMoves.append((self.pos[0]+2, self.pos[1]))
+
+        return validMoves
 
 class Knight(Piece):
     def checkValidMoves(self, gs):
-        pass
+        validMoves = []
+        return validMoves
 
 class Bishop(Piece):
     def checkValidMoves(self, gs):
-        pass
+        validMoves = []
+        return validMoves
 
 class Rook(Piece):
     def checkValidMoves(self, gs):
-        pass
+        validMoves = []
+        return validMoves
 
 class Queen(Piece):
     def checkValidMoves(self, gs):
-        pass
+        validMoves = []
+        return validMoves
 
 class King(Piece):
     def checkValidMoves(self, gs):
-        pass
+        validMoves = []
+        return validMoves
 
 # Not an actual chess piece, just an empty space on the board
 class Space(Piece):
     def checkValidMoves(self, gs):
-        pass
+        validMoves = []
+        return validMoves
