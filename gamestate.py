fd5e7edac8408ced6de99175d1bc4d7057d71145
Roger <tanimowr@mcmaster.ca>
2021-05-15 07:39:28 -0400

    Implemented the ability to update the game board

--- gamestate.py
+++ gamestate.py
@@ -40,7 +40,7 @@ class GameState:
 
         for r in range(8):
             for c in range(8):
-                self.board[r][c].setPos(r, c)
+                self.board[r][c].setPos((r, c))
 
     def getBoard(self):
         return self.board
@@ -50,3 +50,19 @@ class GameState:
 
     def whitesTurn(self):
         return self.whiteToMove
+
+    def updateBoard(self, p1, p2):
+        '''
+        Moves the piece p1 to the position of piece p2 and deletes p2
+            Piece p1: The active piece which is moving
+            Piece p2: The piece being replaced
+        '''
+        pos1 = p1.getPos()
+        pos2 = p2.getPos()
+        p2 = None
+        self.board[pos2[0]][pos2[1]] = p1
+        p1.setPos(pos2)
+        p1.move()
+        self.board[pos1[0]][pos1[1]] = piece.Space("--")
+        self.board[pos1[0]][pos1[1]].setPos(pos1)
+        return
